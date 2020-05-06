75500c0491bf8cef8a1d0f79a514e93688dc29ea
Naoya Otsuka <otknoy@gmail.com>
2020-05-06 11:55:14 +0900

    add HELP and TYPE

--- main.go
+++ main.go
@@ -13,8 +13,16 @@ func main() {
 	http.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
 		metrics, _ := mflight.GetMetrics()
 
+		fmt.Fprintf(w, "# HELP %s_temperature multifunction light temperature\n", namespace)
+		fmt.Fprintf(w, "# TYPE %s_temperature gauge\n", namespace)
 		fmt.Fprintf(w, "%s_temperature %.1f\n", namespace, metrics.Temperature)
+
+		fmt.Fprintf(w, "# HELP %s_humidity multifunction light humidity\n", namespace)
+		fmt.Fprintf(w, "# TYPE %s_humidity gauge\n", namespace)
 		fmt.Fprintf(w, "%s_humidity %.1f\n", namespace, metrics.Humidity)
+
+		fmt.Fprintf(w, "# HELP %s_illuminance multifunction light illuminance\n", namespace)
+		fmt.Fprintf(w, "# TYPE %s_illuminance gauge\n", namespace)
 		fmt.Fprintf(w, "%s_illuminance %d\n", namespace, metrics.Illuminance)
 	})
 
