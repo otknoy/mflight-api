0c51a860835608cd94cb37e99269a615ebc74909
Naoya Otsuka <otknoy@gmail.com>
2020-11-11 22:38:53 +0900

    use github.com/prometheus/client_golang

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module mflight-exporter
 
 go 1.14
+
+require github.com/prometheus/client_golang v1.8.0
--- main.go
+++ main.go
@@ -3,13 +3,13 @@ package main
 import (
 	"fmt"
 	"log"
+	"mflight-exporter/collector"
 	"mflight-exporter/config"
 	"mflight-exporter/mflight"
 	"net/http"
-)
 
-const (
-	namespace = "MultiFunctionLight"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 )
 
 func main() {
@@ -19,22 +19,10 @@ func main() {
 	}
 
 	sensor := mflight.NewMfLight(c.MfLight.URL, c.MfLight.MobileID)
+	col := collector.NewMfLightCollector(sensor)
 
-	http.HandleFunc("/metrics", func(w http.ResponseWriter, r *http.Request) {
-		metrics, _ := sensor.GetMetrics()
-
-		fmt.Fprintf(w, "# HELP %s_temperature multifunction light temperature\n", namespace)
-		fmt.Fprintf(w, "# TYPE %s_temperature gauge\n", namespace)
-		fmt.Fprintf(w, "%s_temperature %.1f\n", namespace, metrics.Temperature)
-
-		fmt.Fprintf(w, "# HELP %s_humidity multifunction light humidity\n", namespace)
-		fmt.Fprintf(w, "# TYPE %s_humidity gauge\n", namespace)
-		fmt.Fprintf(w, "%s_humidity %.1f\n", namespace, metrics.Humidity)
-
-		fmt.Fprintf(w, "# HELP %s_illuminance multifunction light illuminance\n", namespace)
-		fmt.Fprintf(w, "# TYPE %s_illuminance gauge\n", namespace)
-		fmt.Fprintf(w, "%s_illuminance %d\n", namespace, metrics.Illuminance)
-	})
+	prometheus.MustRegister(col)
+	http.Handle("/metrics", promhttp.Handler())
 
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%d", c.Port), nil))
 }
