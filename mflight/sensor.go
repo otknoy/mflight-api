8491f186ddfbd772b21e7d4ac2a6768e928016de
Naoya Otsuka <otknoy@gmail.com>
2020-12-25 21:50:08 +0900

    comment to exported type and function

--- mflight/sensor.go
+++ mflight/sensor.go
@@ -5,16 +5,23 @@ import (
 	"fmt"
 )
 
+// Temperature is value object
 type Temperature float32
+
+// Humidity is value object
 type Humidity float32
+
+// Illuminance is value object
 type Illuminance int16
 
+// Metrics has multiple sensor values
 type Metrics struct {
 	Temperature Temperature `xml:"temp"`
 	Humidity    Humidity    `xml:"humi"`
 	Illuminance Illuminance `xml:"illu"`
 }
 
+// Sensor is interface to get metrics
 type Sensor interface {
 	GetMetrics() (Metrics, error)
 }
@@ -24,10 +31,12 @@ type mfLightSensor struct {
 	mobileID  string
 }
 
+// NewMfLight creates a new MfLight based on mflight server configuration
 func NewMfLight(serverURL, mobileID string) Sensor {
 	return &mfLightSensor{serverURL, mobileID}
 }
 
+// GetMetrics returns current Metrics
 func (l *mfLightSensor) GetMetrics() (Metrics, error) {
 	res, err := getSensorMonitor(l.serverURL, l.mobileID)
 	if err != nil {
