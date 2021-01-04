4d46e9017447df86927074d33372f3dc7a0e9864
Naoya Otsuka <otknoy@gmail.com>
2021-01-04 11:08:44 +0900

    add sensor_test.go

--- infrastructure/mflight/api_test.go
+++ infrastructure/mflight/api_test.go
@@ -1,44 +1,12 @@
 package mflight_test
 
 import (
-	"fmt"
 	"mflight-exporter/infrastructure/mflight"
-	"net/http"
-	"net/http/httptest"
 	"testing"
 )
 
 func TestGetSensorMonitor(t *testing.T) {
-	s := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		if path := r.URL.Path; path != "/SensorMonitorV2.xml" {
-			t.Fatal(path)
-		}
-
-		if qs := r.URL.RawQuery; qs != "x-KEY_MOBILE_ID=test-mobile-id&x-KEY_UPDATE_DATE=" {
-			t.Fatal(qs)
-		}
-
-		fmt.Fprint(
-			w,
-			`
-      <db>
-        <table id="67243">
-          <time>202101030000</time>
-          <unixtime>1609599600</unixtime>
-          <temp>22.0</temp>
-          <humi>43.3</humi>
-          <illu>405</illu>
-        </table>
-        <table id="67244">
-          <time>202101030005</time>
-          <unixtime>1609599900</unixtime>
-          <temp>21.9</temp>
-          <humi>43.0</humi>
-          <illu>406</illu>
-        </table>
-      </db>`,
-		)
-	}))
+	s := NewStubServer(t)
 	defer s.Close()
 
 	res, err := mflight.GetSensorMonitor(s.URL, "test-mobile-id")
