747fc93a8e4068e6162d38a4f7aee1e8b56ebea6
Naoya Otsuka <otknoy@gmail.com>
2021-01-05 21:06:31 +0900

    refactor stubSensor

--- handler/handler_test.go
+++ handler/handler_test.go
@@ -10,20 +10,12 @@ import (
 	"testing"
 )
 
-type stubSensor struct{}
-
-func (s *stubSensor) GetMetrics() (domain.Metrics, error) {
-	return domain.Metrics{
-		Temperature: domain.Temperature(20.0),
-		Humidity:    domain.Humidity(50.0),
-		Illuminance: domain.Illuminance(400),
-	}, nil
+type stubSensor struct {
+	StubGetMetrics func() (domain.Metrics, error)
 }
 
-type stubErrorSensor struct{}
-
-func (s *stubErrorSensor) GetMetrics() (domain.Metrics, error) {
-	return domain.Metrics{}, errors.New("failed to get metrics")
+func (s *stubSensor) GetMetrics() (domain.Metrics, error) {
+	return s.StubGetMetrics()
 }
 
 func TestServeHTTP(t *testing.T) {
@@ -32,7 +24,15 @@ func TestServeHTTP(t *testing.T) {
 
 	want := `{"temperature": 20.0, "humidity": 50.0, "illuminance": 400}`
 
-	h := handler.NewSensorMetricsHandler(&stubSensor{})
+	h := handler.NewSensorMetricsHandler(&stubSensor{
+		func() (domain.Metrics, error) {
+			return domain.Metrics{
+				Temperature: domain.Temperature(20.0),
+				Humidity:    domain.Humidity(50.0),
+				Illuminance: domain.Illuminance(400),
+			}, nil
+		},
+	})
 
 	h.ServeHTTP(got, req)
 
@@ -48,7 +48,11 @@ func TestServeHTTP_sensor_error(t *testing.T) {
 	req := httptest.NewRequest(http.MethodGet, "http://example.com/getSensorMetrics", &bytes.Buffer{})
 	got := httptest.NewRecorder()
 
-	h := handler.NewSensorMetricsHandler(&stubErrorSensor{})
+	h := handler.NewSensorMetricsHandler(&stubSensor{
+		func() (domain.Metrics, error) {
+			return domain.Metrics{}, errors.New("failed to get metrics")
+		},
+	})
 
 	h.ServeHTTP(got, req)
 
