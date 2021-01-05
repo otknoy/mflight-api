f1712fa7cf8f0f38adc20b5743efddfb1c10f5cc
Naoya Otsuka <otknoy@gmail.com>
2021-01-05 21:52:01 +0900

    add application.MetricsCollector

---
+++ application/metrics.go
@@ -0,0 +1,22 @@
+package application
+
+import "mflight-exporter/domain"
+
+// MetricsCollector is interface to collect metrics
+type MetricsCollector interface {
+	CollectMetrics() (domain.Metrics, error)
+}
+
+// NewMetricsCollector creates a new MetricsCollector Based on domain.Sensor
+func NewMetricsCollector(s domain.Sensor) MetricsCollector {
+	return &metricsCollector{s}
+}
+
+type metricsCollector struct {
+	sensor domain.Sensor
+}
+
+// CollectMetrics returns collected metrics
+func (c *metricsCollector) CollectMetrics() (domain.Metrics, error) {
+	return c.sensor.GetMetrics()
+}
