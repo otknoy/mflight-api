5a08a0803503284996a1291ac45d46fa70cd5bc5
Naoya Otsuka <otknoy@gmail.com>
2021-01-10 18:37:03 +0900

    add http_requests_seconds metrics

--- infrastructure/prometheus/middleware/middleware.go
+++ infrastructure/prometheus/middleware/middleware.go
@@ -3,7 +3,20 @@ package middleware
 import (
 	"log"
 	"net/http"
+	"time"
+
+	"github.com/prometheus/client_golang/prometheus"
 )
+
+var (
+	httpReq = prometheus.NewSummary(prometheus.SummaryOpts{
+		Name: "http_requests_seconds",
+	})
+)
+
+func init() {
+	prometheus.MustRegister(httpReq)
+}
 
 type HandlerMetricsMiddleware interface {
 	http.Handler
@@ -18,9 +31,37 @@ type middleware struct {
 }
 
 func (m *middleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+
 	log.Printf("request: %v\n", r.URL.Path)
 
-	m.h.ServeHTTP(w, r)
+	wr := &statusRecoder{
+		status: 0,
+		w:      w,
+	}
 
-	log.Printf("response: %v\n", w)
+	m.h.ServeHTTP(wr, r)
+
+	log.Printf("response: %v\n", wr.status)
+
+	elappsed := time.Now().Sub(start)
+	httpReq.Observe(float64(elappsed))
+}
+
+type statusRecoder struct {
+	status int
+	w      http.ResponseWriter
+}
+
+func (r *statusRecoder) Header() http.Header {
+	return r.w.Header()
+}
+
+func (r *statusRecoder) Write(bytes []byte) (int, error) {
+	return r.w.Write(bytes)
+}
+
+func (r *statusRecoder) WriteHeader(statusCode int) {
+	r.status = statusCode
+	r.w.WriteHeader(statusCode)
 }
