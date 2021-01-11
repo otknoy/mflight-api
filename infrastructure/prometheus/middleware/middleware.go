faa9ef8eb01b6b31e8c279cb778a81f10ad534ab
Naoya Otsuka <otknoy@gmail.com>
2021-01-11 17:14:56 +0900

    remove unnecessary interface

--- infrastructure/prometheus/middleware/middleware.go
+++ infrastructure/prometheus/middleware/middleware.go
@@ -1,16 +1,13 @@
 package middleware
 
 import (
-	"log"
 	"net/http"
 	"time"
 )
 
-type HandlerMetricsMiddleware interface {
-	http.Handler
-}
-
-func NewHandlerMetricsMiddleware(h http.Handler) HandlerMetricsMiddleware {
+// NewHandlerMetricsMiddleware returns a middleware that Wraps the provided http.Handler
+// to observe the request count and total request duration.
+func NewHandlerMetricsMiddleware(h http.Handler) http.Handler {
 	return &middleware{
 		s: make(summaries),
 		h: h,
@@ -25,13 +22,9 @@ type middleware struct {
 func (m *middleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	start := time.Now()
 
-	log.Printf("request: %v\n", r.URL.Path)
-
 	wr := newStatusRecoder(w)
 
 	m.h.ServeHTTP(wr, r)
-
-	log.Printf("response: %v\n", wr.status)
 
 	elappsed := time.Since(start)
 
