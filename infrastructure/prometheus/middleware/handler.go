2e1a95d6e8b01d62af5061916df14f4adfb10349
Naoya Otsuka <otknoy@gmail.com>
2021-01-25 22:37:06 +0900

    move http.Handler middleware to infrastructure/prometheus/middleware/handler.go

---
+++ infrastructure/prometheus/middleware/handler.go
@@ -0,0 +1,37 @@
+package middleware
+
+import (
+	"net/http"
+	"strconv"
+	"time"
+)
+
+// NewHandlerMetricsMiddleware returns a middleware that Wraps the provided http.Handler
+// to observe the request count and total request duration.
+func NewHandlerMetricsMiddleware(h http.Handler) http.Handler {
+	return &httpHandlerMiddleware{
+		h: h,
+	}
+}
+
+type httpHandlerMiddleware struct {
+	h http.Handler
+}
+
+var _ http.Handler = (*httpHandlerMiddleware)(nil)
+
+func (m *httpHandlerMiddleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	start := time.Now()
+
+	wr := newStatusRecoder(w)
+
+	m.h.ServeHTTP(wr, r)
+
+	duration := time.Since(start)
+
+	sv.WithLabelValues(
+		r.Method,
+		r.URL.Path,
+		strconv.Itoa(wr.status),
+	).Observe(duration.Seconds())
+}
--- infrastructure/prometheus/middleware/middleware.go
+++ infrastructure/prometheus/middleware/middleware.go
@@ -1,10 +1,6 @@
 package middleware
 
 import (
-	"net/http"
-	"strconv"
-	"time"
-
 	"github.com/prometheus/client_golang/prometheus"
 )
 
@@ -21,32 +17,4 @@ func init() {
 	)
 
 	prometheus.MustRegister(sv)
-}
-
-// NewHandlerMetricsMiddleware returns a middleware that Wraps the provided http.Handler
-// to observe the request count and total request duration.
-func NewHandlerMetricsMiddleware(h http.Handler) http.Handler {
-	return &middleware{
-		h: h,
-	}
-}
-
-type middleware struct {
-	h http.Handler
-}
-
-func (m *middleware) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	start := time.Now()
-
-	wr := newStatusRecoder(w)
-
-	m.h.ServeHTTP(wr, r)
-
-	duration := time.Since(start)
-
-	sv.WithLabelValues(
-		r.Method,
-		r.URL.Path,
-		strconv.Itoa(wr.status),
-	).Observe(duration.Seconds())
 }
