b505c6634749462a3b07e647c93d8b6ad40d7686
Naoya Otsuka <otknoy@gmail.com>
2021-02-11 09:57:40 +0900

    refactor graceful shutdown HTTP server

--- interfaces/server/server.go
+++ interfaces/server/server.go
@@ -9,37 +9,36 @@ import (
 	"os/signal"
 )
 
-type HTTPServer struct {
-	srv *http.Server
+type server struct {
+	http.Server
 }
 
-func (s *HTTPServer) ListenAndServe(idleConnsClosed chan<- struct{}) {
+func (s *server) ListenAndServeWithGracefulShutdown() <-chan struct{} {
+	idleConnsClosed := make(chan struct{})
+
 	go func() {
 		sigint := make(chan os.Signal, 1)
 		signal.Notify(sigint, os.Interrupt)
 		<-sigint
 
-		if err := s.srv.Shutdown(context.Background()); err != nil {
+		if err := s.Shutdown(context.Background()); err != nil {
 			log.Printf("HTTP server Shutdown: %v", err)
 		}
 
 		close(idleConnsClosed)
 	}()
-	if err := s.srv.ListenAndServe(); err != http.ErrServerClosed {
+	if err := s.ListenAndServe(); err != http.ErrServerClosed {
 		log.Fatalf("HTTP server ListenAndServe: %v", err)
 	}
+
+	return idleConnsClosed
 }
 
-func NewServer(handlers map[string]http.Handler, port int) *HTTPServer {
-	mux := http.NewServeMux()
-	for path, handler := range handlers {
-		mux.Handle(path, handler)
+func NewServer(mux *http.ServeMux, port int) *server {
+	return &server{
+		http.Server{
+			Addr:    fmt.Sprintf(":%d", port),
+			Handler: mux,
+		},
 	}
-
-	srv := &http.Server{
-		Addr:    fmt.Sprintf(":%d", port),
-		Handler: mux,
-	}
-
-	return &HTTPServer{srv}
 }
