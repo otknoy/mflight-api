cb07fd285bc2d916c7150bac677e7dd929452b44
Naoya Otsuka <otknoy@gmail.com>
2021-08-23 21:24:05 +0900

    add domain test

---
+++ domain/metrics_test.go
@@ -0,0 +1,57 @@
+package domain_test
+
+import (
+	"mflight-api/domain"
+	"testing"
+	"time"
+
+	"github.com/google/go-cmp/cmp"
+)
+
+func TestMetricsList(t *testing.T) {
+	t.Run("Last()", func(t *testing.T) {
+		t.Run("should return last element", func(t *testing.T) {
+			l := domain.MetricsList([]domain.Metrics{
+				{
+					Time:        time.Time{},
+					Temperature: 25,
+					Humidity:    62,
+					Illuminance: 412,
+				},
+				{
+					Time:        time.Time{},
+					Temperature: 24,
+					Humidity:    60,
+					Illuminance: 400,
+				},
+			})
+
+			want := domain.Metrics{
+				Time:        time.Time{},
+				Temperature: 24,
+				Humidity:    60,
+				Illuminance: 400,
+			}
+
+			got, err := l.Last()
+
+			if err != nil {
+				t.Fatalf("error must not occur. %v", err)
+			}
+
+			if diff := cmp.Diff(want, got); diff != "" {
+				t.Errorf("differ. \n%s\n", diff)
+			}
+		})
+
+		t.Run("should return error when list has no element", func(t *testing.T) {
+			l := domain.MetricsList([]domain.Metrics{})
+
+			_, err := l.Last()
+
+			if err == nil {
+				t.Fatal("error must occur.")
+			}
+		})
+	})
+}
