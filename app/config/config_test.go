288bf9094e0e60e1411dbc65484647c8013772f9
Naoya Otsuka <otknoy@gmail.com>
2022-03-01 23:27:51 +0900

    fix config test

--- app/config/config_test.go
+++ app/config/config_test.go
@@ -14,7 +14,11 @@ func TestLoad(t *testing.T) {
 	t.Setenv("APP_MFLIGHT_MOBILE_ID", "test-mobileID")
 	t.Setenv("APP_MFLIGHT_CACHE_TTL", "15s")
 
-	got, _ := config.Load()
+	got, err := config.Load()
+
+	if err != nil {
+		t.Errorf("error should not occur.\n%v", err)
+	}
 
 	want := config.AppConfig{
 		Port: 5000,
@@ -30,46 +34,24 @@ func TestLoad(t *testing.T) {
 	}
 }
 
-func TestLoad_cache_ttl(t *testing.T) {
+func TestLoad_returns_error_when_invalid_configuration(t *testing.T) {
 	tests := []struct {
-		ttl  string
-		want time.Duration
+		port, cacheTtl string
 	}{
-		{"1ms", time.Millisecond},
-		{"2s", 2 * time.Second},
-		{"3m", 3 * time.Minute},
-		{"", 0},
-		{"invalid-duration-string", 0},
+		{"5000", "invalid-cache-ttl"},
+		{"invalid-port-number", "15s"},
 	}
 
 	for _, tt := range tests {
-		t.Setenv("APP_PORT", "5000")
+		t.Setenv("APP_PORT", tt.port)
 		t.Setenv("APP_MFLIGHT_URL", "http://example.com:56002")
 		t.Setenv("APP_MFLIGHT_MOBILE_ID", "test-mobileID")
-		t.Setenv("APP_MFLIGHT_CACHE_TTL", tt.ttl)
+		t.Setenv("APP_MFLIGHT_CACHE_TTL", tt.cacheTtl)
 
-		got, _ := config.Load()
+		_, err := config.Load()
 
-		want := config.AppConfig{
-			Port: 5000,
-			MfLight: config.MfLightConfig{
-				URL:      "http://example.com:56002",
-				MobileID: "test-mobileID",
-				CacheTTL: tt.want,
-			},
+		if err == nil {
+			t.Errorf("error should occur.\n%v", tt)
 		}
-
-		if diff := cmp.Diff(want, got); diff != "" {
-			t.Errorf("config differs.\n%v", diff)
-		}
-	}
-}
-
-func TestLoad_return_error_if_port_number_is_invalid(t *testing.T) {
-	t.Setenv("APP_PORT", "invalid-port-number")
-
-	_, err := config.Load()
-	if err == nil {
-		t.Error("error should occur.")
 	}
 }
